5c512cb12477816e1e139a734114b7d8bfb6b7e2
zainab66 <zainab66r@gmail.com>
2021-09-28 22:34:36 -0400

    created login and authorize for users

---
+++ server/middleware/adminAuthorize.js
@@ -0,0 +1,11 @@
+
+
+const is_admin = (req, res, next) => {
+    if (req.user && req.user.is_admin) {
+      next();
+    } else {
+      res.status(401).send({ message: 'Invalid Admin Token' });
+    }
+  };
+
+module.exports = is_admin;
--- server/routes/users.js
+++ server/routes/users.js
@@ -7,20 +7,21 @@ const Child = require('../models/childModel.js');
 const Admin = require('../models/adminModel.js');
 const bcrypt = require('bcrypt');
 const generateToken = require('../utils.js');
-
+const authorize = require("../middleware/authorize")
+const is_parent = require("../middleware/parentAuthorize")
 
 
 
 
 /* GET users listing. */
-router.get(
-  '/seed',
-  expressAsyncHandler(async (req, res) => {
-    //await User.remove({});
-    const createdUsers = await Parent.insertMany(data.users);
-    res.send({ createdUsers });
-  })
-);
+// router.get(
+//   '/seed',
+//   expressAsyncHandler(async (req, res) => {
+//     //await User.remove({});
+//     const createdUsers = await Parent.insertMany(data.users);
+//     res.send({ createdUsers });
+//   })
+// );
 
 
 router.post(
@@ -53,12 +54,12 @@ router.post(
       token: generateToken(createdParent),
     });
 
-   
+
   })
 );
 
 router.post(
-  '/coParent/register',
+  '/coParent/register',authorize,is_parent,
   expressAsyncHandler(async (req, res) => {
     const { email } = req.body;
 
@@ -88,12 +89,12 @@ router.post(
       createBy: createdCoParent.createBy,
       token: generateToken(createdCoParent),
     });
-    
+
   })
 );
 
 router.post(
-  '/child/register',
+  '/child/register',authorize,is_parent,
   expressAsyncHandler(async (req, res) => {
     const { email } = req.body;
 
@@ -151,7 +152,7 @@ router.post(
       is_admin: req.body.is_admin,
 
     });
-    const createdAdmin= await admin.save();
+    const createdAdmin = await admin.save();
     res.send({
       _id: createdAdmin._id,
       firstName: createdAdmin.firstName,
@@ -165,13 +166,113 @@ router.post(
 );
 
 
+router.post(
+  '/admin/signin',
+  expressAsyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (email === '' && password === '') {
+      res.status(401).send({ message: 'Please enter Email and Password' });
+    } else {
+      const admin = await Admin.findOne({ email });
+      if (!admin) {
+        res.status(401).send({ message: 'Admin with this Email does not exist.' });
+      } else {
+        if (admin) {
+          if (password === '') {
+            res.status(401).send({ message: 'Please Enter Password.' });
+          } else {
+            if (await bcrypt.compare(
+              req.body.password,
+              admin.password)) {
+              res.send({
+                _id: admin._id,
+                email: admin.email,
+                is_admin: admin.is_admin,
+                token: generateToken(admin),
+              });
+            } else {
+              res.status(401).send({ message: 'Password is Incorrect.' });
+            }
+          }
+        } else {
+          res.status(401).send({ message: 'Invalid Email or Password' });
+        }
+      }
+    }
 
+  }));
 
+router.post(
+  '/parent/signin',
+  expressAsyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (email === '' && password === '') {
+      res.status(401).send({ message: 'Please enter Email and Password' });
+    } else {
+      const parent = await Parent.findOne({ email });
+      if (!parent) {
+        res.status(401).send({ message: 'Parent with this Email does not exist.' });
+      } else {
+        if (parent) {
+          if (password === '') {
+            res.status(401).send({ message: 'Please Enter Password.' });
+          } else {
+            if (await bcrypt.compare(
+              req.body.password,
+              parent.password)) {
+              res.send({
+                _id: parent._id,
+                email: parent.email,
+                is_parent: parent.is_parent,
+                token: generateToken(parent),
+              });
+            } else {
+              res.status(401).send({ message: 'Password is Incorrect.' });
+            }
+          }
+        } else {
+          res.status(401).send({ message: 'Invalid Email or Password' });
+        }
+      }
+    }
 
+  }));
 
-
-
-
+router.post(
+  '/coParent/signin',
+  expressAsyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (email === '' && password === '') {
+      res.status(401).send({ message: 'Please enter Email and Password' });
+    } else {
+      const coParent = await CoParent.findOne({ email });
+      if (!coParent) {
+        res.status(401).send({ message: 'Co-Parent with this Email does not exist.' });
+      } else {
+        if (coParent) {
+          if (password === '') {
+            res.status(401).send({ message: 'Please Enter Password.' });
+          } else {
+            if (await bcrypt.compare(
+              req.body.password,
+              coParent.password)) {
+              res.json({
+                _id: coParent._id,
+                createBy:coParent.createBy,
+                email: coParent.email,
+                is_co_parent: coParent.is_co_parent,
+                token: generateToken(coParent),
+              });
+            } else {
+              res.status(401).send({ message: 'Password is Incorrect.' });
+            }
+          }
+        } else {
+          res.status(401).send({ message: 'Invalid Email or Password' });
+        }
+      }
+    }
+  }));
 
 module.exports = router;
 
