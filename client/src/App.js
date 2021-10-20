5cc66521c20e89a28af128e1e0fc3244ab903fa6
zainab66 <zainab66r@gmail.com>
2021-10-20 12:30:43 -0400

    update profile pages frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import Home from './screens/Home';
 import signup from './screens/Signup';
 import signin from './screens/Signin';
 import { BrowserRouter, Route } from 'react-router-dom';
-
+import ChildInfo from './screens/ChildInfo';
+import MyInfo from './screens/MyInfo';
+import CoParentInfo from './screens/CoParentInfo';
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <Route path="/" component={Home} exact />
       <Route path="/Signup" component={signup} />
       <Route path="/Signin" component={signin} />
-
+      <Route path="/CoParentInfo" component={CoParentInfo} />
+      <Route path="/ChildInfo" component={ChildInfo} />
+      <Route path="/MyInfo" component={MyInfo} />
     </BrowserRouter>
   );
 }
