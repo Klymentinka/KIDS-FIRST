7f75c125ba4a3a728c87d490c0dddf720885d7bd
Lunettes0910 <tnminh98@gmail.com>
2021-12-04 11:57:11 -0500

    Updated landing page

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,24 +1,19 @@
 import React from 'react'
 import { Button, Dropdown } from 'react-bootstrap'
+import '../css/home.css'
 import logo from '../img/kids_first_logo_beta.png'
-import value_1 from '../img/value_1.png'
-import value_2 from '../img/value_2.png'
-import value_3 from '../img/value_3.png'
-import value_4 from '../img/value_4.png'
-import feature_1 from '../img/feature_1.png'
-import feature_2 from '../img/feature_2.png'
-import feature_3 from '../img/feature_3.png'
-import feature_4 from '../img/feature_4.png'
-import kidsfirst_intro_1 from '../img/kidsfirst_intro_1.png'
-import kidsfirst_intro_2 from '../img/kidsfirst_intro_2.png'
+import value_1 from '../img/home/value_1.png'
+import value_2 from '../img/home/value_2.png'
+import value_3 from '../img/home/value_3.png'
+import feature_1 from '../img/home/feature_1.png'
+import feature_2 from '../img/home/feature_2.png'
+import feature_3 from '../img/home/feature_3.png'
+import video from '../img/home/video_placeholder.png'
 
 export default function Home() {
     return (
         <>
             <header class="banner-section" id="header-banner">
-                <div class="banner-img">
-                    <img src={kidsfirst_intro_1} alt="" />
-                </div>
                 <div class="header-nav">
                     <div class="site-nav">
                         <img src={logo} alt="" />
@@ -40,8 +35,8 @@ export default function Home() {
                     </div>
                 </div>
                 <div class="left-blurb">
-                    <h1>A family solution application</h1>
-                    <p>Bring harmony in your children's lives with the help of one click</p>
+                    <h1>A co-parenting app solution</h1>
+                    <p>Maintain harmony in your kid’s lives with the help of one click</p>
                     <Button className="home-signup-btn-2" href="/Signup">Sign up now</Button>
                 </div>
             </header>
@@ -49,31 +44,23 @@ export default function Home() {
                 <h1 className="value">Kids First Values</h1>
                 <div class="features-prev">
                     <div class="feature-1">
-                        <p>Allow parents to manage all aspects of their children’s lives</p>
+                        <p>Help children have more harmony in their upbringing by reducing conflicts that arise when divorced parents schedule who has the kids.</p>
                         <img src={value_1} alt=""></img>
                     </div>
                     <div class="feature-1">
-                        <p>Mediate Parents’ interactions</p>
+                        <p>Provide a fair and simple co-parenting solution that has less chance for conflict and more time and focus on the children's well-being.</p>
                         <img src={value_2} alt=""></img>
                     </div>
                     <div class="feature-1">
-                        <p>Track actions of each parent to hold them accountable</p>
+                        <p>Allow parents to grow and improve their parenting by making helpful information on child health and Ontario law more accessible.</p>
                         <img src={value_3} alt=""></img>
-                    </div>
-                    <div class="feature-1">
-                        <p>Provide them with general facts on Law and Child Health</p>
-                        <img src={value_4} alt=""></img>
                     </div>
                 </div>
             </section>
             <section class="banner-section" id="middle-banner">
-                <div class="banner-img">
-                    <img src={kidsfirst_intro_2} alt="" />
-                </div>
                 <div class="right-blurb">
                     <h1>Children happiness is our priority</h1>
-                    Provide a fair and simple co-parenting solution that has less chance for conflict and more time and focus on
-                    the children's well-being.
+                    <p>KIDS FIRST aspires to help families with simpler custody scheduling by providing an interactive shared calendar solution that reduces misunderstandings and conflict.</p>
                 </div>
             </section>
             <section class="site-section">
@@ -81,27 +68,23 @@ export default function Home() {
                 <div class="features-prev">
                     <div class="feature-2">
                         <img src={feature_1} alt=""></img>
-                        <p>Mediated Messaging</p>
-                    </div>
-                    <div class="feature-2">
-                        <img src={feature_2} alt=""></img>
-                        <p>Law Pop-Ups</p>
-                    </div>
-                    <div class="feature-2">
-                        <img src={feature_3} alt=""></img>
                         <p>Interactive Calendar</p>
                     </div>
                     <div class="feature-2">
-                        <img src={feature_4} alt=""></img>
-                        <p>Child Info</p>
+                        <img src={feature_2} alt=""></img>
+                        <p>Child Information</p>
+                    </div>
+                    <div class="feature-2">
+                        <img src={feature_3} alt=""></img>
+                        <p>Law Pop-Ups</p>
                     </div>
                 </div>
                 <span class="more-link"><a href="#">More Features</a></span>
             </section>
             <section class="site-section">
-                <h1>Communicate Effectively With Kids First</h1>
-                <div class="feature-3">
-                    <div className="video"></div>
+                <h1>Why Kids First</h1>
+                <div class="feature-3 video">
+                    <img src={video} alt=""></img>
                 </div>
             </section>
             <footer>
